32bc746d039e71602aa7842b939de409e74a122e
rekantofiq <77417254+rekantofiq@users.noreply.github.com>
2021-01-14 22:03:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace birth_age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            int year = today.Year;
+            int age = 0;
+            Console.WriteLine("enter your age ");
+            age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("your age =  " + age);
+            int birth = year - age;
+            Console.WriteLine("your birth =  " + birth);
+        }
+    }
+}
